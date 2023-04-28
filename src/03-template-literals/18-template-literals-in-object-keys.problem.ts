f1de0dfe13e0391fd17ca798d9fa16378215d8d7
“pnowak” <piotr.nowak.25.02.1978@gmail.com>
2023-04-28 10:00:37 +0200

    Solution for 18-template-literals-in-object-keys problem

--- src/03-template-literals/18-template-literals-in-object-keys.problem.ts
+++ src/03-template-literals/18-template-literals-in-object-keys.problem.ts
@@ -2,7 +2,9 @@ import { Equal, Expect } from "../helpers/type-utils";
 
 type TemplateLiteralKey = `${"user" | "post" | "comment"}${"Id" | "Name"}`;
 
-type ObjectOfKeys = unknown;
+type ObjectOfKeys = {
+    [TKey in TemplateLiteralKey]: string
+};
 
 type tests = [
   Expect<
