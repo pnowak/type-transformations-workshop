fcbac67205dd1139840335b13ff501a88ec475ba
“pnowak” <piotr.nowak.25.02.1978@gmail.com>
2023-05-08 10:16:16 +0200

    Solution for 20.1-maybe problem

--- src/03.5-type-helpers-pattern/20.1-maybe.problem.ts
+++ src/03.5-type-helpers-pattern/20.1-maybe.problem.ts
@@ -1,10 +1,11 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-type Maybe = unknown;
+type Maybe<T> = T | null | undefined;
+type MaybeAny = Maybe<any>; // Any destroys this whole union
 
 type tests = [
   Expect<Equal<Maybe<string>, string | null | undefined>>,
   Expect<Equal<Maybe<number>, number | null | undefined>>,
   Expect<Equal<Maybe<boolean>, boolean | null | undefined>>,
-  Expect<Equal<Maybe<null>, null | undefined>>,
+  Expect<Equal<Maybe<null>, null | undefined>>, // null will get collapsed, because if there's two unions at the same type or two union members of the same type, and they get squashed together.
 ];
